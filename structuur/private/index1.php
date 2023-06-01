fc13e98fa221ed7ee48ff7b1ab7d9d182bd52b0b
Roy <34898@ma-web.nl>
2023-06-01 09:41:48 +0200

    a

---
+++ structuur/private/index1.php
@@ -0,0 +1,45 @@
+<?php
+
+$maand = 4;
+switch ($maand) {
+    case 0:
+        echo "Januari";
+        break;
+    case 1:
+        echo "Februari";
+        break;
+    case 2:
+        echo "Maart";
+        break;
+    case 3:
+        echo "April";
+        break;
+    case 4:
+        echo "Mei";
+        break;
+    case 5:
+        echo "Juni";
+        break;
+    case 6:
+        echo "Juli";
+        break;
+    case 7:
+        echo "Augustus";
+        break;
+    case 8:
+        echo "September";
+        break;
+    case 9:
+        echo "Oktober";
+        break;
+    case 10:
+        echo "November";
+        break;
+    case 11:
+        echo "December";
+        break;
+    default:
+        echo "Onbekende maand";
+}
+
+?>
